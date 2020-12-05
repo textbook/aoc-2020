766f9e8560cf777561fab59f131ed6e3ea5dfba9
Jonathan Sharpe <j.r.sharpe+github@gmail.com>
2020-12-05 18:00:20 +0000

    Day 5: Binary Boarding - part 1

---
+++ day05/impl.py
@@ -0,0 +1,47 @@
+#!/usr/bin/env python
+from textwrap import dedent
+import unittest
+
+
+class PuzzleTest(unittest.TestCase):
+
+    def test_examples(self):
+        for in_, out in [
+            ("FBFBBFFRLR", 357),
+            ("BFFFBBFRRR", 567),
+            ("FFFBBBFRRR", 119),
+            ("BBFFBBFRLL", 820),
+        ]:
+            with self.subTest(in_=in_, out=out):
+                self.assertEqual(seat_id(in_), out)
+
+    def test_puzzle(self):
+        self.assertEqual(puzzle(dedent("""
+            FBFBBFFRLR
+            BFFFBBFRRR
+            FFFBBBFRRR
+            BBFFBBFRLL
+        """.strip())), 820)
+
+
+def bisect(code, front):
+    options = list(range(2 ** len(code)))
+    for char in code:
+        if char == front:
+            options = options[:len(options)//2]
+        else:
+            options = options[len(options)//2:]
+    return options[0]
+
+
+def seat_id(seat):
+    return (bisect(seat[:7], "F") * 8) + bisect(seat[7:], "L")
+
+
+def puzzle(data):
+    return max(seat_id(seat) for seat in data.split())
+
+
+if __name__ == "__main__":
+    with open("input.txt") as f:
+        print(puzzle(f.read().strip()))
