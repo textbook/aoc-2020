e1ea6270fd50229c2c40de9e98af5a5262d7aa3f
Jonathan Sharpe <j.r.sharpe+github@gmail.com>
2020-12-08 11:21:15 +0000

    Refactoring

--- day08/impl.py
+++ day08/impl.py
@@ -1,4 +1,5 @@
 #!/usr/bin/env python3
+from collections import namedtuple
 from os.path import dirname
 import unittest
 
@@ -21,40 +22,51 @@ class PuzzleTest(unittest.TestCase):
         self.assertEqual(8, puzzle(self.example))
 
 
+class InfiniteLoop(Exception):
+    pass
+
+
+Operation = namedtuple("Operation", ("accumulate", "jump"))
+
+
+def create_operation(op, val):
+    if op == "acc":
+        return Operation(accumulate=val, jump=1)
+    elif op == "jmp":
+        return Operation(accumulate=0, jump=val)
+    return Operation(accumulate=0, jump=1)
+
+
+CHANGES = dict(jmp="nop", nop="jmp")
+
+
 def execute(program):
-    """Return the value of the accumulator when the program ends.
-
-    Return None if the program loops.
-
-    """
+    """Return the value of the accumulator when the program ends."""
     accumulator = 0
     pointer = 0
     visited = set()
 
     while True:
         if pointer in visited:
-            return None
+            raise InfiniteLoop
         if pointer >= len(program):
             return accumulator
-        op = program[pointer]
         visited.add(pointer)
-        if op.startswith("nop"):
-            pointer += 1
-        elif op.startswith("jmp"):
-            pointer += int(op.split()[1])
-        elif op.startswith("acc"):
-            accumulator += int(op.split()[1])
-            pointer += 1
+        op = create_operation(*program[pointer])
+        accumulator += op.accumulate
+        pointer += op.jump
 
 
 def puzzle(program):
-    for index, line in enumerate(program):
-        if line.startswith(("nop", "jmp")):
-            new_program = program[:]
-            op, acc = line.split()
-            new_program[index] = f"{'nop' if op == 'jmp' else 'jmp'} {acc}"
-            if (result := execute(new_program)) is not None:
-                return result
+    lines = [(op, int(val)) for op, val in map(str.split, program)]
+    for index, (op, val) in enumerate(lines):
+        if op in CHANGES:
+            new_program = lines[:]
+            new_program[index] = CHANGES[op], val
+            try:
+                return execute(new_program)
+            except InfiniteLoop:
+                continue
 
 
 if __name__ == "__main__":
